a808651d032fb3cb31a654ed202dc3924e933b5a
Oleksandr Zakhozhyi <sannymasters@gmail.com>
2025-04-24 09:52:21 +0300

    added Dockerfile

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,10 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
 app.include_router(api_router, prefix="/api")
